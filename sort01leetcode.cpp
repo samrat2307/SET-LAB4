92566f21e699c03bf0968e13c100970cf76b9b2e
samrat2307 <85503379+samrat2307@users.noreply.github.com>
2023-02-24 09:30:43 +0530

    Add files via upload

---
+++ sort01leetcode.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+void printArray(int arr[],int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+   //in this question we will bring all the 0' to left and all 1's to right
+   int i=0;
+   int j=n-1;
+   while(i<j)
+   {
+      if(arr[i]==0 && i<j) //if 0 is at i'th position and 1 is 
+         i++;
+      if(arr[j]==1 && i<j)
+         j--;
+      if(i<j)
+      {
+         swap(arr[i],arr[j]);
+         i++;
+         j--;
+      }
+   }
+}
+
+int main()
+{
+    int arr[]={0,1,0,1,0,0,1};
+    int n=7;
+    sort012(arr,7);
+    printArray(arr,7);
+}
